ac6b834cd0ea89bce95d8e9914a0fe7679d1f392
geyClown <66467307+geyClown@users.noreply.github.com>
2023-05-05 10:21:45 +0700

    5-5
    fix view model

--- Data_View_Model/ItemVM.cs
+++ Data_View_Model/ItemVM.cs
@@ -5,7 +5,6 @@
         public int ItemId { get; set; }
         public int UserId { get; set; }
         public string ItemTitle { get; set; } = null!;
-
         public double? ItemSalePrice { get; set; }
         public int ItemShareAmount { get; set; }
         public string Image { get; set; } = null!;
@@ -42,23 +41,5 @@
             return ItemVM;
         }
 
-        public static ItemVM ItemForOrderToViewModel(Models.Item item)
-        {
-
-            var ItemVM = new ItemVM()
-            {
-                ItemId = item.ItemId,
-                UserId = item.UserId,
-                ItemTitle = item.ItemTitle,
-                ItemSalePrice = item.ItemSalePrice,
-                ItemShareAmount = item.ItemShareAmount,
-
-                Image = item.Image,
-                ItemStatus = item.ItemStatus,
-            };
-
-
-            return ItemVM;
-        }
     }
 }
